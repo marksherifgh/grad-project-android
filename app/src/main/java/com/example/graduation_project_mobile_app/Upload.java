03f1fc93b2ed7fe7ddc57daf83405ff562171631
marksherifgh <marc-sherif@hotmail.com>
2021-04-09 20:28:06 +0200

    feature: add spinner of displacement/velocity/acceleration to activity_upload.xml

--- app/src/main/java/com/example/graduation_project_mobile_app/Upload.java
+++ app/src/main/java/com/example/graduation_project_mobile_app/Upload.java
@@ -4,8 +4,11 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.VideoView;
 
 import com.jjoe64.graphview.GraphView;
@@ -19,6 +22,12 @@ public class Upload extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload);
+        Spinner spinner = (Spinner) findViewById(R.id.graph_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.graph_spinner, R.layout.spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
         GraphView graph = (GraphView) findViewById(R.id.graph);
         LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
                 new DataPoint(0, 1),
@@ -27,7 +36,12 @@ public class Upload extends AppCompatActivity {
                 new DataPoint(3, 2),
                 new DataPoint(4, 6)
         });
+        graph.getGridLabelRenderer().setGridColor(Color.WHITE);
+        graph.getGridLabelRenderer().setVerticalLabelsColor(Color.WHITE);
+        graph.getGridLabelRenderer().setHorizontalLabelsColor(Color.WHITE);
+        graph.getGridLabelRenderer().reloadStyles();
         graph.addSeries(series);
+
 //      TODO: Set onCharge for cutoff frequency to change the graph
 //      TODO: get graphview by id
 //      TODO: pass data from opencv to graphview
