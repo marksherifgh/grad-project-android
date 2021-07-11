c2371a56f7acca399b9920c84654b02cd848fd45
marksherifgh <marc-sherif@hotmail.com>
2021-07-11 20:13:11 +0200

    Add Icon and change the app name to MotionTracker ASU and change help activity to add help video

--- app/src/main/java/com/example/graduation_project_mobile_app/How.java
+++ app/src/main/java/com/example/graduation_project_mobile_app/How.java
@@ -1,22 +1,27 @@
 package com.example.graduation_project_mobile_app;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.widget.Button;
-import android.content.Intent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.VideoView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class How extends AppCompatActivity {
 
     public Button getAruco;
+    public VideoView video;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_how);
         getAruco = (Button) findViewById(R.id.getAruco);
+        video = (VideoView) findViewById(R.id.videoview);
+        video.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.help);
+        video.start();
         getAruco.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
